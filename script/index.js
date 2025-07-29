63d69970293da09d06d99a0fd903bdd4dc00eb5a
Stotskiy_Nikita <LenovoNikita2017@yandex.ru>
2025-07-29 08:18:56 +0700

    Add a non-adaptive hero section, an adaptive section for productions, and start working on the services section.

--- script/index.js
+++ script/index.js
@@ -3,9 +3,15 @@ const sidebar = document.querySelector('#sidebar');
 const menu_button = document.querySelector('.button__mobile__menu');
 const overlay = document.querySelector('.overlay');
 const icon_button = menu_button.querySelector("i");
+const map_button = document.querySelector('#map-button');
+const headerHeight = header.offsetHeight;
 
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('main').style.paddingTop = headerHeight + 'px';
+})
 
 menu_button.addEventListener('click', e => {
+    console.log('clicked');
     let isActive = e.target.classList.toggle('active');
     sidebar.classList.toggle('active');
     overlay.classList.toggle('active');
@@ -13,16 +19,16 @@ menu_button.addEventListener('click', e => {
     menu_button.setAttribute("aria-expanded", isActive);
     sidebar.setAttribute("aria-hidden", !isActive);
 
-
-    const headerHeight = header.offsetHeight;
-
     if(isActive){
         sidebar.style.top = headerHeight + 'px';
         sidebar.style.height = `calc(100vh - ${headerHeight}px)`;
 
+        map_button.style.display = 'none';
         icon_button.classList.remove("bi-list");
         icon_button.classList.add("bi-x");
     }else {
+        map_button.style.display = 'inline-block';
+
         icon_button.classList.remove("bi-x");
         icon_button.classList.add("bi-list");
     }
@@ -34,8 +40,18 @@ overlay.addEventListener('click', e => {
     menu_button.setAttribute("aria-expanded","false");
     sidebar.setAttribute("aria-hidden", "true");
 
+    map_button.style.display = 'inline-block';
     sidebar.style.top = "0";
     sidebar.style.height = `calc(100vh)`;
     icon_button.classList.remove("bi-x");
     icon_button.classList.add("bi-list");
+})
+
+
+document.addEventListener('DOMContentLoaded', e => {
+    console.log('DOMContentLoaded');
+})
+
+document.addEventListener('click', e => {
+    console.log(document.getSelection());
 })
