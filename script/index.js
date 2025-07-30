470dfbc39c0e69f113c4d921f0038399adf82292
Stotskiy_Nikita <LenovoNikita2017@yandex.ru>
2025-07-30 15:50:49 +0700

    add new section

--- script/index.js
+++ script/index.js
@@ -8,10 +8,11 @@ const headerHeight = header.offsetHeight;
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('main').style.paddingTop = headerHeight + 'px';
+    sidebar.style.top = headerHeight + 'px';
+    sidebar.style.height = `calc(100vh - ${headerHeight}px)`;
 })
 
 menu_button.addEventListener('click', e => {
-    console.log('clicked');
     let isActive = e.target.classList.toggle('active');
     sidebar.classList.toggle('active');
     overlay.classList.toggle('active');
@@ -20,38 +21,54 @@ menu_button.addEventListener('click', e => {
     sidebar.setAttribute("aria-hidden", !isActive);
 
     if(isActive){
-        sidebar.style.top = headerHeight + 'px';
-        sidebar.style.height = `calc(100vh - ${headerHeight}px)`;
-
         map_button.style.display = 'none';
         icon_button.classList.remove("bi-list");
         icon_button.classList.add("bi-x");
     }else {
         map_button.style.display = 'inline-block';
-
         icon_button.classList.remove("bi-x");
         icon_button.classList.add("bi-list");
     }
 })
 
 overlay.addEventListener('click', e => {
+    closeSidebar()
+})
+
+
+function closeSidebar() {
     sidebar.classList.remove('active');
     overlay.classList.remove('active');
     menu_button.setAttribute("aria-expanded","false");
     sidebar.setAttribute("aria-hidden", "true");
-
     map_button.style.display = 'inline-block';
-    sidebar.style.top = "0";
     sidebar.style.height = `calc(100vh)`;
     icon_button.classList.remove("bi-x");
     icon_button.classList.add("bi-list");
-})
+}
+
+function scrollToSection(button_selector, section_selector, redirect_page) {
+    let buttons = document.querySelectorAll(button_selector);
+    let section = document.querySelector(section_selector);
+
+    buttons.forEach(button => {
+        button.addEventListener('click', e => {
+
+                if(!document.querySelector(section_selector) && redirect_page != null) {
+                    window.location.href = redirect_page
+                }
+
+                if(sidebar.classList.contains('active')) {
+                    closeSidebar();
+                }
+
+                window.scroll({
+                    top: section.offsetTop - headerHeight + 20,
+                    behavior: 'smooth'
+                });
+        })
+    })
+}
 
 
-document.addEventListener('DOMContentLoaded', e => {
-    console.log('DOMContentLoaded');
-})
-
-document.addEventListener('click', e => {
-    console.log(document.getSelection());
-})
+scrollToSection(".btn-map", ".map__section", "index.html")
